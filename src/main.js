6d268e346a7f73386ca1b519717343e3c233f825
daehyun <detail54@nate.com>
2022-06-15 15:34:33 +0900

    feat: err handling

--- src/main.js
+++ src/main.js
@@ -2,65 +2,23 @@
 // @ts-check
 
 const express = require('express')
-const bodyParser = require('body-parser')
-
-const userRouter = express.Router()
 
 const app = express()
-app.use(bodyParser.json())
+app.use(express.json())
 app.set('views', 'src/views')
 app.set('view engine', 'pug')
 
 const PORT = 5000
 
-const USERS = {
-  15: {
-    nickname: 'foo',
-  },
-  16: {
-    nickname: 'bar',
-  },
-}
-
-userRouter.get('/', (req, res) => {
-  res.send('Root - GET')
-})
-
-userRouter.param('id', (req, res, next, value) => {
-  // @ts-ignore
-  req.user = USERS[value]
-  next()
-})
-
-userRouter.get('/:id', (req, res) => {
-  const resMimeType = req.accepts(['json', 'html'])
-
-  if (resMimeType === 'json') {
-    // @ts-ignore
-    res.send(req.user)
-  } else if (resMimeType === 'html') {
-    res.render('user-profile', {
-      // @ts-ignore
-      nickname: req.user.nickname,
-    })
-  }
-})
-
-userRouter.post('/', (req, res) => {
-  res.send('Root - POST')
-})
-
-userRouter.post('/:id/nickname', (req, res) => {
-  // @ts-ignore
-  const { user } = req
-  const { nickname } = req.body
-
-  user.nickname = nickname
-  res.send(USERS)
-})
+const userRouter = require('./routers/user')
 
 app.use('/users', userRouter)
 app.use('/public', express.static('src/public'))
+
+app.use((err, req, res, next) => {
+  res.statusCode = err.statusCode || 500
+  res.send(err.message)
+})
 
 app.get('/', (req, res) => {
   res.render('index', {
