97da4e8e39d6e67e85a3364118e6dcb8ad0b574e
daehyun <detail54@nate.com>
2022-06-17 09:45:02 +0900

    feat: Add socket

--- packages/chating/src/main.js
+++ packages/chating/src/main.js
@@ -1,8 +1,12 @@
 const Koa = require('koa')
 const Pug = require('koa-pug')
 const path = require('path')
+const route = require('koa-route')
+const websockify = require('koa-websocket')
+const serve = require('koa-static')
+const mount = require('koa-mount')
 
-const app = new Koa()
+const app = websockify(new Koa())
 
 // eslint-disable-next-line no-new
 new Pug({
@@ -10,8 +14,33 @@ new Pug({
   app,
 })
 
+app.use(mount('/public', serve('src/public')))
+
 app.use(async (ctx) => {
   await ctx.render('index')
 })
+
+app.ws.use(
+  route.all('/ws', (ctx) => {
+    ctx.websocket.on('message', (data) => {
+      const { message, nickname } = JSON.parse(data)
+
+      const { server } = app.ws
+
+      if (!server) {
+        return
+      }
+
+      server.clients.forEach((client) => {
+        client.send(
+          JSON.stringify({
+            message,
+            nickname,
+          })
+        )
+      })
+    })
+  })
+)
 
 app.listen(5000)
---
+++ packages/chating/src/public/client.js
@@ -0,0 +1,67 @@
+// @ts-check
+
+;(() => {
+  const socket = new WebSocket(`ws://${window.location.host}/ws`)
+  const formEl = document.getElementById('form')
+  const chatsEL = document.getElementById('chats')
+  /** @type {HTMLInputElement | null} */
+  // @ts-ignore
+  const inputEL = document.getElementById('input')
+
+  if (!formEl || !inputEL || !chatsEL) {
+    throw new Error('Init failed!')
+  }
+
+  /**
+   * @typedef Chat
+   * @property {string} nickname
+   * @property {string} message
+   */
+
+  /**
+   * @type {Chat []}
+   */
+  const chats = []
+
+  const adjectives = ['멋진', '훌륭한', '친절한', '새침한']
+  const animals = ['물범', '사자', '사슴', '돌고래', '독수리']
+
+  /**
+   * @param {string[]} array
+   * @returns {string}
+   */
+  function pickRandom(array) {
+    const randomIndex = Math.floor(Math.random() * array.length)
+    const result = array[randomIndex]
+    if (!result) {
+      throw new Error('array length is 0.')
+    }
+
+    return result
+  }
+
+  const myNickname = `${pickRandom(adjectives)} ${pickRandom(animals)}`
+
+  formEl.addEventListener('submit', (event) => {
+    event.preventDefault()
+    socket.send(
+      JSON.stringify({
+        nickname: myNickname,
+        message: inputEL.value,
+      })
+    )
+    inputEL.value = ''
+  })
+
+  socket.addEventListener('message', (event) => {
+    chats.push(JSON.parse(event.data))
+
+    chatsEL.innerHTML = ''
+
+    chats.forEach(({ message, nickname }) => {
+      const div = document.createElement('div')
+      div.innerText = `${nickname}: ${message}`
+      chatsEL.appendChild(div)
+    })
+  })
+})()
