c938c6a526b46edf47134ed3565bf94b71d3a10b
daehyun <detail54@nate.com>
2022-06-21 14:46:37 +0900

    feat: get github issues cli

--- packages/cli/src/main.js
+++ packages/cli/src/main.js
@@ -2,17 +2,33 @@
 require('dotenv').config()
 
 const { GITHUB_ACCESS_TOKEN } = process.env
-console.log('TOKEN:', GITHUB_ACCESS_TOKEN)
 
 const { program } = require('commander')
+const { Octokit } = require('octokit')
 
 program.version('0.0.1')
+
+const octokit = new Octokit({ auth: GITHUB_ACCESS_TOKEN })
+
+program
+  .command('me')
+  .description('Check my profile')
+  .action(async () => {
+    const {
+      data: { login },
+    } = await octokit.rest.users.getAuthenticated()
+    console.log('Hello, %s', login)
+  })
 
 program
   .command('list-bugs')
   .description('List issues with bug label')
   .action(async () => {
-    console.log('List bugs!')
+    const result = await octokit.rest.issues.listForRepo({
+      owner: 'detail54',
+      repo: 'node-study',
+    })
+    console.log(result)
   })
 
 program
